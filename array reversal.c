ac4b4ef93aa504f655c4e261d2035829a9f89ada
chetannnnnnnnnnnn <chetankumar5102009@gmail.com>
2024-11-11 23:17:52 +0530

    completed

--- array reversal.c
+++ array reversal.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int num, *arr, i;
+int main(){
+    int tmp;
+    int num, *arr, i, *arr2,index;
     scanf("%d", &num);
     arr = (int*) malloc(num * sizeof(int));
+    arr2 = (int*) malloc(num * sizeof(int));
     for(i = 0; i < num; i++) {
         scanf("%d", arr + i);
     }
-
-
-  
-
+    for (int i = 0; i < num; i++){
+        index = (num-1)-i;
+     arr2[index] = arr[i];
+     
+    }
     for(i = 0; i < num; i++)
-        printf("%d ", *(arr + i));
+        printf("%d ", *(arr2 + i));
     return 0;
 }
+
